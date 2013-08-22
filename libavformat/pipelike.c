297b9e5d29035bf53d20158f0461d595f7ce27ba
Nikolay Morev <kolyuchiy@gmail.com>
2013-08-22 19:05:37 +0400

    Modify file.c with support for read-only streaming files

--- libavformat/pipelike.c
+++ libavformat/pipelike.c
@@ -43,15 +43,6 @@
 #include "os_support.h"
 #include "url.h"
 
-/* Some systems may not have S_ISFIFO */
-#ifndef S_ISFIFO
-#  ifdef S_IFIFO
-#    define S_ISFIFO(m) (((m) & S_IFMT) == S_IFIFO)
-#  else
-#    define S_ISFIFO(m) 0
-#  endif
-#endif
-
 /* standard file protocol */
 
 typedef struct FileContext {
@@ -59,15 +50,12 @@ typedef struct FileContext {
     int fd;
     int trunc;
     int blocksize;
+    AVIOInterruptCB eof_callback;
 } FileContext;
+void pipelike_set_eof_callback(URLContext *h, AVIOInterruptCB eof_callback);
 
 static const AVOption file_options[] = {
     { "truncate", "truncate existing files on write", offsetof(FileContext, trunc), AV_OPT_TYPE_INT, { .i64 = 1 }, 0, 1, AV_OPT_FLAG_ENCODING_PARAM },
-    { "blocksize", "set I/O operation maximum block size", offsetof(FileContext, blocksize), AV_OPT_TYPE_INT, { .i64 = INT_MAX }, 1, INT_MAX, AV_OPT_FLAG_ENCODING_PARAM },
-    { NULL }
-};
-
-static const AVOption pipe_options[] = {
     { "blocksize", "set I/O operation maximum block size", offsetof(FileContext, blocksize), AV_OPT_TYPE_INT, { .i64 = INT_MAX }, 1, INT_MAX, AV_OPT_FLAG_ENCODING_PARAM },
     { NULL }
 };
@@ -79,28 +67,19 @@ static const AVClass file_class = {
     .version    = LIBAVUTIL_VERSION_INT,
 };
 
-static const AVClass pipe_class = {
-    .class_name = "pipe",
-    .item_name  = av_default_item_name,
-    .option     = pipe_options,
-    .version    = LIBAVUTIL_VERSION_INT,
-};
-
 static int file_read(URLContext *h, unsigned char *buf, int size)
 {
     FileContext *c = h->priv_data;
     int r;
     size = FFMIN(size, c->blocksize);
     r = read(c->fd, buf, size);
-    return (-1 == r)?AVERROR(errno):r;
-}
+    if (r == 0 && !ff_check_interrupt(&c->eof_callback)) {
+        // При достижении конца файла не прекращаем чтение, а ждем, пока данные не
+        // появятся.
 
-static int file_write(URLContext *h, const unsigned char *buf, int size)
-{
-    FileContext *c = h->priv_data;
-    int r;
-    size = FFMIN(size, c->blocksize);
-    r = write(c->fd, buf, size);
+        return AVERROR(EAGAIN);
+    }
+
     return (-1 == r)?AVERROR(errno):r;
 }
 
@@ -108,35 +87,6 @@ static int file_get_handle(URLContext *h)
 {
     FileContext *c = h->priv_data;
     return c->fd;
-}
-
-static int file_check(URLContext *h, int mask)
-{
-    int ret = 0;
-    const char *filename = h->filename;
-    av_strstart(filename, "file:", &filename);
-
-    {
-#if HAVE_ACCESS && defined(R_OK)
-    if (access(filename, F_OK) < 0)
-        return AVERROR(errno);
-    if (mask&AVIO_FLAG_READ)
-        if (access(filename, R_OK) >= 0)
-            ret |= AVIO_FLAG_READ;
-    if (mask&AVIO_FLAG_WRITE)
-        if (access(filename, W_OK) >= 0)
-            ret |= AVIO_FLAG_WRITE;
-#else
-    struct stat st;
-    ret = stat(filename, &st);
-    if (ret < 0)
-        return AVERROR(errno);
-
-    ret |= st.st_mode&S_IRUSR ? mask&AVIO_FLAG_READ  : 0;
-    ret |= st.st_mode&S_IWUSR ? mask&AVIO_FLAG_WRITE : 0;
-#endif
-    }
-    return ret;
 }
 
 #if CONFIG_PIPELIKE_PROTOCOL
@@ -150,26 +100,29 @@ static int file_open(URLContext *h, const char *filename, int flags)
 
     av_strstart(filename, "pipelike:", &filename);
 
-    if (flags & AVIO_FLAG_WRITE && flags & AVIO_FLAG_READ) {
-        access = O_CREAT | O_RDWR;
-        if (c->trunc)
-            access |= O_TRUNC;
-    } else if (flags & AVIO_FLAG_WRITE) {
-        access = O_CREAT | O_WRONLY;
-        if (c->trunc)
-            access |= O_TRUNC;
-    } else {
-        access = O_RDONLY;
-    }
+    access = O_RDONLY;
 #ifdef O_BINARY
     access |= O_BINARY;
 #endif
-    fd = avpriv_open(filename, access, 0666);
+    do {
+        if (fd == -1 && errno == ENOENT) {
+            // Небольшая пауза перед повторной попыткой.
+            usleep(100000);
+        }
+
+        fd = avpriv_open(filename, access, 0666);
+
+        // Файл может еще не появиться на диске в момент начала чтения, поэтому
+        // повторяем попытку открыть.
+    } while (fd == -1 && errno == ENOENT &&
+             ! ff_check_interrupt(&h->interrupt_callback) &&
+             ! ff_check_interrupt(&c->eof_callback));
+
     if (fd == -1)
         return AVERROR(errno);
     c->fd = fd;
 
-    h->is_streamed = !fstat(fd, &st) && S_ISFIFO(st.st_mode);
+    h->is_streamed = 1;
 
     return 0;
 }
@@ -183,7 +136,7 @@ static int64_t file_seek(URLContext *h, int64_t pos, int whence)
     if (whence == AVSEEK_SIZE) {
         struct stat st;
         ret = fstat(c->fd, &st);
-        return ret < 0 ? AVERROR(errno) : (S_ISFIFO(st.st_mode) ? 0 : st.st_size);
+        return ret < 0 ? AVERROR(errno) : st.st_size;
     }
 
     ret = lseek(c->fd, pos, whence);
@@ -197,55 +150,23 @@ static int file_close(URLContext *h)
     return close(c->fd);
 }
 
+void pipelike_set_eof_callback(URLContext *h, AVIOInterruptCB eof_callback)
+{
+    FileContext *c = h->priv_data;
+    c->eof_callback = eof_callback;
+}
+
 URLProtocol ff_pipelike_protocol = {
     .name                = "pipelike",
     .url_open            = file_open,
     .url_read            = file_read,
-    .url_write           = file_write,
+    .url_write           = NULL,
     .url_seek            = file_seek,
     .url_close           = file_close,
     .url_get_file_handle = file_get_handle,
-    .url_check           = file_check,
+    .url_check           = NULL,
     .priv_data_size      = sizeof(FileContext),
     .priv_data_class     = &file_class,
 };
 
 #endif /* CONFIG_PIPELIKE_PROTOCOL */
-
-#if CONFIG_PIPE_PROTOCOL
-
-static int pipe_open(URLContext *h, const char *filename, int flags)
-{
-    FileContext *c = h->priv_data;
-    int fd;
-    char *final;
-    av_strstart(filename, "pipe:", &filename);
-
-    fd = strtol(filename, &final, 10);
-    if((filename == final) || *final ) {/* No digits found, or something like 10ab */
-        if (flags & AVIO_FLAG_WRITE) {
-            fd = 1;
-        } else {
-            fd = 0;
-        }
-    }
-#if HAVE_SETMODE
-    setmode(fd, O_BINARY);
-#endif
-    c->fd = fd;
-    h->is_streamed = 1;
-    return 0;
-}
-
-URLProtocol ff_pipe_protocol = {
-    .name                = "pipe",
-    .url_open            = pipe_open,
-    .url_read            = file_read,
-    .url_write           = file_write,
-    .url_get_file_handle = file_get_handle,
-    .url_check           = file_check,
-    .priv_data_size      = sizeof(FileContext),
-    .priv_data_class     = &pipe_class,
-};
-
-#endif /* CONFIG_PIPE_PROTOCOL */
